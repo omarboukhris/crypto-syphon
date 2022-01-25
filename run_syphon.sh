33ccae5797ffcab78506c71438d77da630f6edf2
omar <omarboukhris@gmail.com>
2022-01-25 16:26:36 +0100

    removed coin from sh script

--- run_syphon.sh
+++ run_syphon.sh
@@ -3,6 +3,4 @@ python main.py BTC USD & \
 python main.py ETH USD & \
 python main.py DOGE USD & \
 python main.py LUNA USD & \
-python main.py LTC USD & \
-python main.py ATOM USD
-
+python main.py LTC USD 
